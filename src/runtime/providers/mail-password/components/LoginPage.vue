38e49c91cec699e233217bb91f92e4c1d0c1c159
NaXiDaXi <maximilian@blue-panda.de>
2024-07-29 09:47:37 +0200

    feat!: upgrade @antify/ui-module peerdependency

--- src/runtime/providers/mail-password/components/LoginPage.vue
+++ src/runtime/providers/mail-password/components/LoginPage.vue
@@ -1,77 +1,76 @@
 <script setup lang="ts">
 import {
-	ref,
-	useUi,
-	reactive,
-	useFetch,
-	useRoute,
-	showError,
-	useUiClient,
-	onBeforeUnmount,
-	onMounted
+  ref,
+  reactive,
+  useFetch,
+  useRoute,
+  showError,
+  onBeforeUnmount,
+  onMounted
 } from '#imports';
 import {type LocationAsRelativeRaw} from '#vue-router';
 import {validator as _validator, type Input} from '../glue/login.post';
+import {State} from '#ui-module';
 
 defineProps<{
-	forgotPasswordRoute: LocationAsRelativeRaw
+  forgotPasswordRoute: LocationAsRelativeRaw
 
-	/**
-	 * Improves ux: If the user is already logged in, the user
-	 * get redirected without authentication process.
-	 */
-	isAlreadyLoggedIn?: () => boolean
+  /**
+   * Improves ux: If the user is already logged in, the user
+   * get redirected without authentication process.
+   */
+  isAlreadyLoggedIn?: () => boolean
 }>();
 const emit = defineEmits(['login']);
 const route = useRoute();
 const formData = ref<Input>({
-	email: '',
-	password: '',
-	// TODO:: rename inviteToken to something cryptic short in query
-	inviteToken: route.query?.inviteToken as string,
+  email: '',
+  password: '',
+  // TODO:: rename inviteToken to something cryptic short in query
+  inviteToken: route.query?.inviteToken as string,
 });
 const validator = reactive(_validator);
 const loginError = ref<string | null>(null);
 const {
-	execute,
-	status,
+  execute,
+  status,
 } = useFetch('/api/authentication-module/providers/mail-password/login', {
-	method: 'post',
-	body: formData,
-	watch: false,
-	immediate: false,
-	onResponse({response}) {
-		if (response.status === 500) {
-			return showError(response._data)
-		}
+  method: 'post',
+  body: formData,
+  watch: false,
+  immediate: false,
+  onResponse({response}) {
+    if (response.status === 500) {
+      return showError(response._data);
+    }
 
-		if (response.status === 200) {
-			if (response._data?.success) {
-				return emit('login');
-			}
+    if (response.status === 200) {
+      if (response._data?.success) {
+        return emit('login');
+      }
 
-			if (response._data?.invalidCredentials) {
-				loginError.value = response._data?.invalidCredentials;
-			}
-		}
-	}
+      if (response._data?.invalidCredentials) {
+        loginError.value = response._data?.invalidCredentials;
+      }
+    }
+  }
 });
 
 async function onLogin() {
-	loginError.value = null;
+  loginError.value = null;
 
-	validator.validate(formData.value, 'client');
+  validator.validate(formData.value, 'client');
 
-	if (validator.hasErrors()) {
-		return;
-	}
+  if (validator.hasErrors()) {
+    return;
+  }
 
-	await execute();
+  await execute();
 }
 
 onMounted(() => validator.reset());
 onBeforeUnmount(() => {
-	validator.reset();
+  validator.reset();
 });
 </script>
 
@@ -99,7 +98,7 @@ onBeforeUnmount(() => {
           <AntAlert
             v-if="loginError"
             :expanded="true"
-            :color-type="useUi().ColorType.danger"
+            :state="State.danger"
             :icon="false"
             :dismiss-btn="false"
           >
@@ -117,17 +116,14 @@ onBeforeUnmount(() => {
             @validate="val => validator.fieldMap.email.validator.validate(val, formData.email, 'client')"
           />
 
-          <!--					TODO:: Replace with AntPasswordInput-->
-          <AntTextInput
+          <AntPasswordInput
             v-model="formData.password"
             label="Password"
             name="password"
-            type="password"
             :disabled="status === 'pending'"
             :errors="validator.fieldMap.password.validator.getErrors()"
             @validate="val => validator.fieldMap.password.validator.validate(val, formData.password, 'client')"
           />
-
 
           <div class="text-right">
             <NuxtLink
@@ -141,7 +137,7 @@ onBeforeUnmount(() => {
           <AntButton
             :expanded="true"
             :filled="true"
-            :color-type="useUi().ColorType.primary"
+            :state="State.primary"
             :disabled="status === 'pending'"
             @click="() => onLogin()"
           >
@@ -155,6 +151,6 @@ onBeforeUnmount(() => {
 
 <style scoped>
 .login-bg {
-	background: radial-gradient(100% 100% at 50% 0%, #F1F5F9 37.5%, #7DD3FC 100%);
+  background: radial-gradient(100% 100% at 50% 0%, #F1F5F9 37.5%, #7DD3FC 100%);
 }
 </style>
